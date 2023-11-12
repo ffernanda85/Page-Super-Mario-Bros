8614905c4ff932d54e7ddbce77bf4bb9faae8086
ffernanda85 <f.fernanda85@gmail.com>
2023-11-12 18:06:40 -0300

    feat: created functions form

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-const anoDaVirada = 2023
 
-console.log(anoDaVirada);
+let form = document.querySelector("#contact")
+let gradient = document.querySelector("#gradient-form")
+
+function showForm() {
+    form.style.left = "600px"
+    gradient.style.visibility = "visible"
+}
+
+function outForm() {
+    form.style.left = "-400px"
+    gradient.style.visibility = "hidden"
+}
